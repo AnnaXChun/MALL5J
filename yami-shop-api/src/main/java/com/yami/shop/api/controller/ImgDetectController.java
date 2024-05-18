d073a099e7d7199899807d434c7413431ce4ebbb
吴淑茂 <2412718175@qq.com>
2024-05-18 15:18:17 +0800

    image搜索历史完成

--- yami-shop-api/src/main/java/com/yami/shop/api/controller/ImgDetectController.java
+++ yami-shop-api/src/main/java/com/yami/shop/api/controller/ImgDetectController.java
@@ -23,6 +23,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
 
@@ -38,21 +40,15 @@ public class ImgDetectController {
     @Autowired
     private ImgService imgService;
 
-    @GetMapping("/test")
-    public String getPayment() {
-        return restTemplate.getForObject(PAYMENT_URL + "/test", String.class);
-    }
 
-    @PostMapping("/detect")
-    public ResponseEntity<Image> getDetectImg(@RequestParam("file") MultipartFile file, @RequestParam String user_id) throws IOException {
+    @PostMapping("/detect/{userid}")
+    public ResponseEntity<Image> getDetectImg(@RequestParam("file") MultipartFile file, @PathVariable String userid) throws IOException {
         ByteArrayResource fileResource = new ByteArrayResource(file.getBytes()) {
             @Override
             public String getFilename() {
                 return file.getOriginalFilename();
             }
         };
-
-
         MultiValueMap<String, Object> requestBody = new LinkedMultiValueMap<>();
         requestBody.add("file", fileResource);
         HttpHeaders headers = new HttpHeaders();
@@ -62,11 +58,15 @@ public class ImgDetectController {
 
         String pythonServiceUrl = PAYMENT_URL+"/api/img";
         ResponseEntity<Image> response = restTemplate.postForEntity(pythonServiceUrl, requestEntity,Image.class);
-        Objects.requireNonNull(response.getBody()).setUser_id(user_id);
-        System.out.println(response);
+        Image image = response.getBody();
+        LocalDateTime now = LocalDateTime.now();
+        if (image != null) {
+            image.setUser_id(userid);
+            image.setSearch_time(now);
+        }
         if (response.getStatusCode().is2xxSuccessful()) {
             imgService.saveImage(response.getBody());
-            return response;
+            return new ResponseEntity<>(image, HttpStatus.OK);
         } else {
             System.out.println("Failed to call Python API");
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -75,6 +75,7 @@ public class ImgDetectController {
 
     @PostMapping("/history")
     public ServerResponseEntity<List<ImageDto>> getHistory(@RequestParam String userId){
+        System.out.println(userId);
         List<ImageDto>imageDtoList = imgService.getHistory(userId);
         return ServerResponseEntity.success(imageDtoList);
     }
--- yami-shop-bean/src/main/java/com/yami/shop/bean/model/Image.java
+++ yami-shop-bean/src/main/java/com/yami/shop/bean/model/Image.java
@@ -6,17 +6,17 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.sql.Date;
+import java.time.LocalDateTime;
 
 @Data
 @TableName("history")
 public class Image implements Serializable {
 
     //用户id
-
     private String user_id;
 
     //图片id
-    private String image_id;
+    private Integer image_id;
 
     //图片识别的名称
     private String file_name;
@@ -26,7 +26,6 @@ public class Image implements Serializable {
 
     //搜索的时间
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date search_time;
-
+    private LocalDateTime search_time;
 
 }
