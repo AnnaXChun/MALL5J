dd79ddb0cc24a4b9f22cc17b390aecaaeaf730e9
吴淑茂 <2412718175@qq.com>
2024-05-18 12:32:59 +0800

    Changes

--- yami-shop-sys/src/main/java/com/yami/shop/sys/dao/SysUserMapper.java
+++ yami-shop-sys/src/main/java/com/yami/shop/sys/dao/SysUserMapper.java
@@ -12,6 +12,7 @@ package com.yami.shop.sys.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yami.shop.sys.model.SysUser;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -20,22 +21,23 @@ import java.util.List;
  * @author lanhai
  * 系统用户
  */
+@Mapper
 public interface SysUserMapper extends BaseMapper<SysUser> {
-	
+
 	/**
 	 * 查询用户的所有权限
 	 * @param userId  用户ID
 	 * @return
 	 */
 	List<String> queryAllPerms(@Param("userId") Long userId);
-	
+
 	/**
 	 * 根据用户id 批量删除用户
 	 * @param userIds
 	 * @param shopId
 	 */
 	void deleteBatch(@Param("userIds") Long[] userIds, @Param("shopId") Long shopId);
-	
+
 	/**
 	 * 根据用户名获取管理员用户
 	 * @param username
